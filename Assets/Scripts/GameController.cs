130d07aabd9f5558ade204619a5c8a1da1d117b9
Scott Bromander <scott@primeacademy.io>
2016-07-17 21:49:03 -0500

    collect orbs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public Transform wall;
+	public Transform player;
+	public Transform orb;
+
 	private int[][] level = new int[][]
 	{
 		new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -21,25 +25,45 @@ public class GameController : MonoBehaviour {
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
-		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1},
-		new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+		new int[]{1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
 		new int[]{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-		new int[]{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+		new int[]{1, 0, 2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
 		new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
-	public Transform wall;
+
 
 	void BuildLevel(){
 		GameObject dynamicParent = GameObject.Find ("Dynamic Objects");
 
 		for (int yPos = 0; yPos < level.Length; yPos++) {
 			for (int xPos = 0; xPos < (level [yPos]).Length; xPos++) {
-				if (level [yPos] [xPos] == 1) {
-					Transform newObject = Instantiate (wall, new Vector3 (xPos, (level.Length - yPos), 0), wall.rotation) as Transform;
+
+				Transform toCreate = null;
+
+				switch(level[yPos][xPos]){
+					case 0:
+						break;
+					case 1:
+					toCreate = wall;
+						break;
+					case 2:
+					toCreate = player;
+						break;
+					case 3:
+					toCreate = orb;
+						break;
+					default:
+						print ("Invalid number: " + (level [yPos] [xPos]).ToString ());
+						break;
+				}
+
+				if(toCreate != null){
+					Transform newObject = Instantiate (toCreate, new Vector3 (xPos, (level.Length - yPos), 0), wall.rotation) as Transform;
 					newObject.parent = dynamicParent.transform;
 				}
 			}
---
+++ Assets/Scripts/OrbBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbBehaviour : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		Destroy (this.gameObject);
+	}
+}
